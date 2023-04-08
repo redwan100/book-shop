f6889637d34db09b38dce108c72ee5b1032d8b04
Redwan islam <redwanislam2760@gmail.com>
2023-04-08 13:11:53 +0600

    added go back functonality

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,28 +1,17 @@
 import React, { useState } from 'react'
-import { useLoaderData, useNavigation } from 'react-router-dom'
+import { useLoaderData, useNavigation, useNavigate } from "react-router-dom";
 import Book from './Book'
 import LoadingSpinner from '../LoadingSpinner'
 
 
-// const Books = () => {
-//    const navigation = useNavigation();
-//    if (navigation.state === "loading") {
-//      return <LoadingSpinner />;
-//    }
-//   const {books} = useLoaderData();
-//   return (
-//     <div className='grid gap-4 grid-cols-[repeat(auto-fit,_minmax(230px,_1fr))] mt-2'>
-//       {
-//         books.map(book =><Book {...book}/>)
-//       }
-//     </div>
-//   )
-// }
+
 const Books = () => {
   const navigation = useNavigation();
      if (navigation.state === "loading") {
        return <LoadingSpinner />;
      }
+
+     const navigate = useNavigate()
   const { books: initialBooks } = useLoaderData();
   const [searchQuery, setSearchQuery] = useState("");
   const [books, setBooks] = useState(initialBooks);
@@ -33,10 +22,12 @@ const Books = () => {
     const res = await fetch(URL);
     const { books } = await res.json();
     setBooks(books);
+    setSearchQuery('')
   };
 
-  console.log("init", initialBooks);
-  console.log("search ", books);
+  const handleBack = ()=>{
+    navigate(-1)
+  }
 
   return (
     <div className="my-4">
@@ -56,7 +47,35 @@ const Books = () => {
           Search
         </button>
       </form>
-      <h2>Books</h2>
+
+      {books.length === 0 && (
+        <div className="relative">
+          <button
+            onClick={handleBack}
+            className="bg-gray-200 py-1 pr-2 pl-1 rounded-md font-medium inline-flex items-center justify-center m-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+            Back
+          </button>
+
+          <p className="text-center mt-11 text-4xl font-semibold">
+            No result found
+          </p>
+        </div>
+      )}
       <div className="grid gap-4 grid-cols-[repeat(auto-fit,_minmax(230px,_1fr))] mt-2">
         {books.map((book) => (
           <Book key={book.isbn13} {...book} />
